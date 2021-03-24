3e0f51a82878abd4624ae72d8cfbea7dc985a9ba
JP <jp.touron@gmail.com>
2021-03-24 19:16:09 +0100

    setup unit tests for unicode converter
    On branch take1-unicodetextconverter-excercises

--- src/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
+++ src/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
@@ -1,5 +1,9 @@
-
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Xunit;
+
 namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
 {
     public class HikerTest
@@ -7,8 +11,86 @@ namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
         [Fact]
         public void Foobar()
         {
-            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter("foobar.txt");
-            Assert.Equal("fixme", converter.GetFilename());
+            const string fileNameWithPath = "./foobar.txt";
+            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(fileNameWithPath);
+            Assert.Equal(fileNameWithPath, converter.GetFilename());
         }
+
+        [Theory]
+        [InlineData(new object[] { "foobar.txt" })]
+        [InlineData(new object[] { "foobar1.txt" })]
+        [InlineData(new object[] { "foobar2.txt" })]
+        [InlineData(new object[] { "foobar3.txt" })]
+        public void Foobar2(string fileName)
+        {
+            string fileNameWithPath = $"./UnicodeFileToHtmlTextConverter.Tests/{fileName}";
+
+            var contents = "";
+
+            //var expectedCount = contents.Split('\n').Length+1;
+            var expectedCount = 0;
+
+            using (var fr = new System.IO.StreamReader(fileNameWithPath))
+            {
+                var line = fr.ReadLine();
+                while (line != null)
+                {
+                    expectedCount++;
+                    line = fr.ReadLine();
+                }
+
+                expectedCount++;
+            }
+
+            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(fileNameWithPath);
+
+            var result = converter.ConvertToHtml();
+
+
+
+
+            var actualCount = result.Split("<br />").Length;
+
+            Assert.Equal(expectedCount, actualCount);
+        }
+
+        [Theory]
+        [InlineData(new object[] { "foobar4.txt", new char[] { '<', '>', '&', '\"', '\'' },
+                                                  new string[] { "&amp;lt;", "&amp;gt;", "&amp;", "&quot;", "&quot;" } })]
+        public void Foobar3(string fileName, char[] availableChars, string[] expectedEncodings)
+        {
+            string fileNameWithPath = $"./UnicodeFileToHtmlTextConverter.Tests/{fileName}";
+
+            var contents = "";
+
+            using (var fr = new System.IO.StreamReader(fileNameWithPath))
+                contents = fr.ReadToEnd();
+
+            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(fileNameWithPath);
+
+            var result = converter.ConvertToHtml();
+
+            var lines = contents.Split("\r\n");
+
+            foreach (var line in lines)
+            {
+
+                var theChar = availableChars.Single(x => x == Convert.ToChar(line));
+
+                var charIdx = availableChars.ToList().FindIndex(x => theChar == x);
+
+                var expectedEncoding = expectedEncodings[charIdx];
+
+                var actualEncoding = result.Split("<br />")[charIdx];
+
+                Assert.Equal(expectedEncoding, actualEncoding);
+            }
+
+
+        }
+
+
+
     }
 }
+
